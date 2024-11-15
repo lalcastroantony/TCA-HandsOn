64f7ac8b8ffea81c79fcb012f70c9b04bcf9eef6
Lal Castro <lal.castro@icloud.com>
2024-11-15 23:44:20 +0530

    Better child parent communication

--- TCA-handsOn/ContactsFeature.swift
+++ TCA-handsOn/ContactsFeature.swift
@@ -9,67 +9,84 @@ import ComposableArchitecture
 import SwiftUI
 
 struct Contact: Equatable, Identifiable {
-  let id: UUID
-  var name: String
+    let id: UUID
+    var name: String
 }
 
 
 @Reducer
 struct ContactsFeature {
-  @ObservableState
-  struct State: Equatable {
-    var contacts: IdentifiedArrayOf<Contact> = []
-  }
-  enum Action {
-    case addButtonTapped
-  }
-  var body: some ReducerOf<Self> {
-    Reduce { state, action in
-      switch action {
-      case .addButtonTapped:
-        // TODO: Handle action
-        return .none
-      }
+    @ObservableState
+    struct State: Equatable {
+        @Presents var addContact: AddContactFeature.State?
+        var contacts: IdentifiedArrayOf<Contact> = []
     }
-  }
+    enum Action {
+        case addButtonTapped
+        case addContact(PresentationAction<AddContactFeature.Action>)
+    }
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+            case .addButtonTapped:
+                state.addContact = AddContactFeature.State(
+                    contact: .init(id: UUID(), name: "")
+                )
+                return .none
+            case .addContact(.presented(.delegate(.saveContact(let contact)))):
+                state.contacts.append(contact)
+                return .none
+            case .addContact:
+                return .none
+            }
+        }
+        .ifLet(\.$addContact, action: \.addContact) {
+            AddContactFeature()
+        }
+    }
 }
 
 struct ContactsView: View {
-  let store: StoreOf<ContactsFeature>
-  
-  var body: some View {
-    NavigationStack {
-      List {
-        ForEach(store.contacts) { contact in
-          Text(contact.name)
+    @Bindable var store: StoreOf<ContactsFeature>
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(store.contacts) { contact in
+                    Text(contact.name)
+                }
+            }
+            .navigationTitle("Contacts")
+            .toolbar {
+                ToolbarItem {
+                    Button {
+                        store.send(.addButtonTapped)
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(item: $store.scope(state: \.addContact, action: \.addContact)) { addContactStore in
+                NavigationStack {
+                    AddContactView(store: addContactStore)
+                }
+            }
         }
-      }
-      .navigationTitle("Contacts")
-      .toolbar {
-        ToolbarItem {
-          Button {
-            store.send(.addButtonTapped)
-          } label: {
-            Image(systemName: "plus")
-          }
-        }
-      }
     }
-  }
 }
 
 #Preview {
-  ContactsView(
-    store: Store(
-      initialState: ContactsFeature.State(
-        contacts: [
-          Contact(id: UUID(), name: "Blob"),
-          Contact(id: UUID(), name: "Blob Jr"),
-          Contact(id: UUID(), name: "Blob Sr"),
-        ]
-      )
-    ) {
-      ContactsFeature()
-    }
-  )
+    ContactsView(
+        store: Store(
+            initialState: ContactsFeature.State(
+                contacts: [
+                    Contact(id: UUID(), name: "Blob"),
+                    Contact(id: UUID(), name: "Blob Jr"),
+                    Contact(id: UUID(), name: "Blob Sr"),
+                ]
+            )
+        ) {
+            ContactsFeature()
+        }
+    )
 }
